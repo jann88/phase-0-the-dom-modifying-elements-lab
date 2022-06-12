9936ee85048679559ea8306d449fe21d0378c250
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-12 13:11:10 +0300

    commit tree 2868b27da5dd70037cf3635ec11189b6207c85e2

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const test = 'hi';
+document.querySelector(main).remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = 'flatiron school is the champion';
+newHeader.className = 'victory'
